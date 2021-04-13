1f892b52041db51613d729f5b06878fa801336c2
201865009 <fikriindriawan1@gmail.com>
2021-04-13 16:12:27 +0900

    UtsFikri

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'BMICalculator.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Kalkulator BMI',
+    home: new BMICalculator(),
+  ));
+}
+
